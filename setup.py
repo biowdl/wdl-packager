0496d3d06d4d934b7463437cf5bd591a6c2274b9
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-12-06 10:21:42 +0100

    also support 3.8

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License"
     ],
     python_requires=">=3.6",  # Also needed for miniwdl
